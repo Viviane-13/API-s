ae73a27d3e574cd69a4f281b10411266e6ba322e
Viviane <oviviane772@gmail.com>
2022-02-01 09:07:53 -0300

    Adicionando o typescript e incrementando a forma de desenvolver

--- ms-authentication/src/index.ts
+++ ms-authentication/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Aplicação executando na porta 3000');
+});
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'sucesso total!' });
+});
+
